eb601c931ebc78c2462f0fa1d3427c004d028f59
ZihaoWang617 <wang.zihao10@northeastern.edu>
2026-04-06 10:25:24 -0700

    feat: add code reviewer with structured prompt and multiline input.

---
+++ llm-basics/code_reviewer.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key = api_key)
+messages = [
+    {"role": "system", "content": """You are a python code reviewer.
+     ##Task
+     Review the user's code and identify issues.
+     
+     ##Output Format
+     Line [number]: [issue description]
+     Fixed code: [fixed code]
+     
+     ##Example
+     user's code: def add(a,b): return a+b
+     output:
+     Line 1: Missing hints.
+     Fixed code: def add(a: int, b: int) -> int:
+     """}     
+]
+
+def get_multiline_input() -> str:
+    print("enter your code(type 'END' on a new line to stop)")
+    lines = []
+    while True:
+        line = input()
+        if line.lower() == 'q':
+            return 'q'
+        elif line.lower() == "end":
+            break
+        lines.append(line)
+    return "\n".join(lines)
+
+
+while True:
+    question = get_multiline_input()
+    if question.lower() == 'q':
+        print("Exiting the program.")
+        break
+    try:
+        messages.append({"role": "user", "content": question})
+        response = client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = messages,
+            temperature = 0,
+        )
+        ai_reply = response.choices[0].message.content
+        messages.append({"role": "assistant", "content": ai_reply})
+        print(ai_reply)
+    except Exception as e:
+        print(f"Error: {e}")
+        break
+
